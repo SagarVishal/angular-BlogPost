b5372933457939a8ff4d90ae4cea82f0b465b405
Vishal Sagar <59005440+Vishalsag@users.noreply.github.com>
2021-03-01 13:36:16 +0530

    Added blog post (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post.modal'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mean-course';
+  storedPosts: Post[] = [];
+
+  onPostAdded(post: any){
+    this.storedPosts.push(post);
+  }
 }
